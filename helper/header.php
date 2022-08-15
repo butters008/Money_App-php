9ab55deefbbb776b6c95ac2c2c9e82b67d4a805e
Butters <k.butterfield08@gmail.com>
2022-08-14 23:15:40 -0500

    Got outline Done
    Got Outline Done

---
+++ helper/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style/gridLayout.css">
+  <link rel="stylesheet" href="style/baseStyle.css">
+  <title>fms-app</title>
+</head>
+<body>
+<header class="headerContent">
+  <div id="siteLogo">
+    <img src="images/farmLogo.png" alt="farmlogo" style="padding-bottom: 10px;">
+      <h1 style="padding-left: 25px;font-size: xxx-large;">FMS</h1>
+  </div>
+  <div id="headerSearch">
+
+  </div>
+  <div id="headerAccount">
+
+  </div>
+</header>
+<main class="mainContentContainer">
+  <nav id="navContainer">
+    <ul class="navULContainer">
+      <li class="navButton"><a href="index.php">Index</a></li>
+      <li class="navButton"><a href="../herd/addAnimal">Add Cow or Bull</a></li>
+      <li class="navButton"><a href="../herd/calfInfo">Add Calf</a></li>
+      <li class="navButton"><a href="../herd/list">List</a></li>
+      <li class="navButton account"><a href="../login/login">Login</a></li>
+      <li class="navButton account"><a href="../login/logout">Logout</a></li>
+    </ul>
+  </nav>  
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+  include "helper/header.php";
+?>
+
+<section id="mainContent">
+
+</section>
+<div id="sideContent">
+
+</div>
+
+
+
+<?php 
+  include "helper/footer.php";
+?>
